78ba217af45eeffa0b69865ae9057ce9f19215fc
nian0114 <268078545@qq.com>
2017-10-09 22:53:59 +0800

    新增 用户界面的通知

--- app/src/main/java/com/lyk/busgrade/Notifications.java
+++ app/src/main/java/com/lyk/busgrade/Notifications.java
@@ -1,18 +1,35 @@
 package com.lyk.busgrade;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by nian on 2017/10/9.
  */
 
 public class Notifications  extends AppCompatActivity {
+
+    public String url = "http://lyk.ttotoo.xyz/notice.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,18 @@ public class Notifications  extends AppCompatActivity {
         actionBar.setTitle(R.string.notice);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        TextView textView = (TextView) findViewById(R.id.notice_tw);
+
+        manageException();// 处理抛出异常
+
+        String myString = null;
+        myString = posturl(url);
+
+        String html = myString;
+        Document doc = Jsoup.parse(html);
+
+        textView.setText(doc.text());
 
     }
 
@@ -42,6 +71,55 @@ public class Notifications  extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * 获取参数指定的网页代码，将其返回给调用者，由调用者对其解析 返回String
+     */
+    public String posturl(String url) {
+        InputStream is = null;
+        String result = "";
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+        } catch (Exception e) {
+            return "Fail to establish http connection!" + e.toString();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+
+            result = sb.toString();
+        } catch (Exception e) {
+            return "Fail to convert net stream!";
+        }
+
+        return result;
+    }
+
+    public void manageException() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads().detectDiskWrites().detectNetwork() // or
+                // .detectAll()
+                // for
+                // all
+                // detectable
+                // problems
+                .penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects().penaltyLog().penaltyDeath()
+                .build());
     }
 
 
